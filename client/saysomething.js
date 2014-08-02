390a6c253be79d64312e74c696c91f3488fe74af
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-08-02 23:21:26 +0300

    save userid with message for later use

--- client/saysomething.js
+++ client/saysomething.js
@@ -2,7 +2,7 @@ Template.saysomething.events({
 	'submit form[name=saysomething]': function(e, tmpl) {
 		e.preventDefault()
 		var data = getFormData('form[name=saysomething]')
-		Messages.insert({fulldate: new Date(), date: getDate(), name: Session.get('username'), message: data.message, aho: []})
+		Messages.insert({user: localStorage.getItem('userId'), date: getDate(), name: Session.get('username'), message: data.message, aho: []})
 		$('form[name=saysomething] .message').val('')
 	}
 })
