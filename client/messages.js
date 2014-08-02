1aa68142495050fd358d1ee6c44754c85f5b6a23
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-08-02 17:20:25 +0300

    initial aho

--- client/messages.js
+++ client/messages.js
@@ -1,5 +1,19 @@
 Template.messages.helpers({
 	messages: function() {
 		return Messages.find()
+	},
+	aho: function() {
+		return this.aho.length
+	},
+	style: function() {
+		console.log($.inArray(Session.get("username"), this.aho))
+		if ($.inArray(Session.get("username"), this.aho) > -1)
+			return "background-color: rgba(0,255,0,0.3); -webkit-border-radius: 10px; border-radius: 10px; border: 1px solid rgba(0,0,0,0.3)"
+	}
+})
+
+Template.messages.events({
+	'click .line': function(e, tmple) {
+		Messages.update(e.currentTarget.id, {$push: {aho: Session.get("username")}})
 	}
 })
--- client/saysomething.js
+++ client/saysomething.js
@@ -2,6 +2,6 @@ Template.saysomething.events({
 	'submit form[name=saysomething]': function(e, tmpl) {
 		e.preventDefault()
 		var data = getFormData('form[name=saysomething]')
-		Messages.insert({date: getDate(), name: Session.get('username'), message: data.message})
+		Messages.insert({date: getDate(), name: Session.get('username'), message: data.message, aho: []})
 	}
 })
