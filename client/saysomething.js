88e2578539ede410ebac38a346033d9ed44cc8dc
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-08-04 11:47:50 +0300

    join name and message fields

--- client/saysomething.js
+++ client/saysomething.js
@@ -2,7 +2,8 @@ Template.saysomething.events({
 	'submit form[name=saysomething]': function(e, tmpl) {
 		e.preventDefault()
 		var data = getFormData('form[name=saysomething]')
-		Messages.insert({timestamp: Date.now(), user: localStorage.getItem('userId'), date: getDate(), name: Session.get('username'), message: data.message, aho: []})
+		console.log(data)
+		Messages.insert({timestamp: Date.now(), user: localStorage.getItem('userId'), date: getDate(), name: data.name, message: data.message, aho: []})
 		$('form[name=saysomething] .message').val('')
 	}
 })
