e1c2c3ffb95b205cde9963a600a20e93975b4cee
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-08-09 21:39:13 +0300

    disable autoreload

--- client/controller/index.js
+++ client/controller/index.js
@@ -5,3 +5,9 @@ Accounts.ui.config({
 Meteor.startup(function(){
 	Session.set('circleTopic', 'maincircle')
 })
+
+
+// Disable automatic reload on file change
+Meteor._reload.onMigrate(function(reloadFunction) {
+	return [false];
+});
