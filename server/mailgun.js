362fffd95d807f661ce88405aad6bdd8a7cc7045
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-09-10 15:29:03 +0300

    gmail spam evade && fix self chat emails

--- server/mailgun.js
+++ server/mailgun.js
@@ -16,10 +16,11 @@ Meteor.startup(function () {
 
 				// This if defines what circles the user can actually read
 				if (
+				    var split = circle._id.split(',')
 					(
 						circle.type == 'open' // Public chat
 						||
-						(circle.type == '4eyes' && circle._id.indexOf(userId) > -1) // 4eyes and is participant
+						(circle.type == '4eyes' && circle._id.indexOf(userId) > -1 && (split.length === 1 || split[0] != split[1])) // 4eyes and is participant but not self-chat
 						||
 						(circle.type == 'closed' && isset(circle.hasAccess) && isset(circle.hasAccess[userId])) // Has access to private chat
 						||
@@ -65,71 +66,77 @@ Meteor.startup(function () {
 emailReminders = function(collection) {
 	console.log("")
 	console.log("Start emailing")
+	var timeout = 0
 
 	_.each(collection, function(doc){
-		console.log(doc)
+		timeout = timeout + 5000
+		// Hoping to bypass google spam this way
+		Meteor.setTimeout(function(){
+			console.log('Timestamp: ' + new Date.now())
+			console.log(doc)
 
-		// Add lastEmail to user
-		Meteor.users.update(doc.userId, {$set: {lastEmail: Date.now()}})
+			// Add lastEmail to user
+			Meteor.users.update(doc.userId, {$set: {lastEmail: Date.now()}})
 
-		// Make body
-		var username = Meteor.users.findOne(doc.userId).username
-		var Digital = new Date()
-		var hours = Digital.getHours()
+			// Make body
+			var username = Meteor.users.findOne(doc.userId).username
+			var Digital = new Date()
+			var hours = Digital.getHours()
 
-		// Configure message below to your own.
-		if (hours>=5&&hours<=11) {
-			var hello = 'Tere hommikust, ' + username
-			var regards = 'Head päeva,'
-		}
-		else if (hours>=12&&hours<=17) {
-			var hello = 'Tere päevast, ' + username
-			var regards = 'Head päeva jätku,'
-		}
-		else if (hours>=18&&hours<=21) {
-			var hello = 'Tere õhtust, ' + username
-			var regards = 'Head ööd,'
-		}
-		else if (hours>=22&&hours<=23) {
-			var hello = 'Tere hilist õhtust, ' + username
-			var regards = 'Head ööd,'
-		}
-		else {
-			var hello = 'Tere tere, öökoll'
-			var regards = 'Parem on, kui lähed Tingi lehele hommikul,'
-		}
-
-		var body = '<p>'+hello+'</p>\
-		<br>\
-		<p>Keegi on viimase 24 tunni jooksul midagi uut Ting.ee-s jutustanud, järsku sind huvitab?</p>\
-		<br>\
-		<p><a href="http://ting.ee">ting.ee</a></p>\
-		<br>\
-		<p>'+regards+'</p>\
-		<p>Tingi kirjatuvi</p>\
-		<br>\
-		<p>P.S. Kahjuks ei ole hetkel e-maili sätteid võimalik muuta. Aga andke teada oma soovidest Arenduse teema all!</p>\
-		<p>P.S.S. Siia emailile vastates ei näe seda hetkel veel keegi.</p>';
-
-		Meteor.http.post(process.env.MAILGUN_API_URL + '/' + process.env.MAILGUN_DOMAIN + '/messages', 
-			{
-				auth:"api:" + process.env.MAILGUN_API_KEY,
-				params: {"from":"Tingi Kirjatuvi <kirjatuvi@ting.ee>",
-					"to":[doc.email],
-					"subject": "Sul on lugemata juttu",
-					"html": body,
-				}
-			},
-			function(error, result) {
-				if(error){
-					console.log("Error: " + error)
-				} else {
-					console.log("Email sent")
-				}
+			// Configure message below to your own.
+			if (hours>=5&&hours<=11) {
+				var hello = 'Tere hommikust, ' + username
+				var regards = 'Head päeva,'
 			}
-		);
+			else if (hours>=12&&hours<=17) {
+				var hello = 'Tere päevast, ' + username
+				var regards = 'Head päeva jätku,'
+			}
+			else if (hours>=18&&hours<=21) {
+				var hello = 'Tere õhtust, ' + username
+				var regards = 'Head ööd,'
+			}
+			else if (hours>=22&&hours<=23) {
+				var hello = 'Tere hilist õhtust, ' + username
+				var regards = 'Head ööd,'
+			}
+			else {
+				var hello = 'Tere tere, öökoll'
+				var regards = 'Parem on, kui lähed Tingi lehele hommikul,'
+			}
+
+			var body = '<p>'+hello+'</p>\
+			<br>\
+			<p>Keegi on viimase 24 tunni jooksul midagi uut Ting.ee-s jutustanud, järsku sind huvitab?</p>\
+			<br>\
+			<p><a href="http://ting.ee">ting.ee</a></p>\
+			<br>\
+			<p>'+regards+'</p>\
+			<p>Tingi kirjatuvi</p>\
+			<br>\
+			<p>P.S. Kahjuks ei ole hetkel e-maili sätteid võimalik muuta. Aga andke teada oma soovidest Arenduse teema all!</p>\
+			<p>P.S.S. Siia emailile vastates ei näe seda hetkel veel keegi.</p>';
+
+			Meteor.http.post(process.env.MAILGUN_API_URL + '/' + process.env.MAILGUN_DOMAIN + '/messages', 
+				{
+					auth:"api:" + process.env.MAILGUN_API_KEY,
+					params: {"from":"Tingi Kirjatuvi <kirjatuvi@ting.ee>",
+						"to":[doc.email],
+						"subject": "Sul on lugemata juttu",
+						"html": body,
+					}
+				},
+				function(error, result) {
+					if(error){
+						console.log("Error: " + error)
+					} else {
+						console.log("Email sent")
+					}
+				}
+			);
+		}, timeout)
 	})
-	console.log("Emailing complete")
+	console.log("Emails queued")
 	console.log("")
 	
 }
