2c37752d08e85325eb8d4ba7c88ba038c91a1200
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-09-26 09:22:53 +0300

    fix accessFriends

--- server/methods.js
+++ server/methods.js
@@ -1,14 +1,15 @@
 Meteor.methods({
 	inviteIn: function(currentUserId, targetUserId) {
-		var targetUser = Meteor.users.findOne(targetUserId)
 		var data = {}
 		data[currentUserId]
 
 		// if accessFriends doesn't exist
+		var targetUser = Meteor.users.findOne(targetUserId)
 		if (!isset(targetUser.profile) || !isset(targetUser.profile.accessFriends))
 			Meteor.users.update(targetUserId, {$set: {'profile.accessFriends': []}})
 			
 		// If current user is not friend already
+		targetUser = Meteor.users.findOne(targetUserId)
 		if (!_.contains(targetUser.profile.accessFriends, currentUserId))
 			Meteor.users.update(targetUserId, {$push: {'profile.accessFriends': currentUserId}})
 		else {
@@ -17,7 +18,8 @@ Meteor.methods({
 		}
 
 		// Is user ready to get access?
-		var friends = Meteor.users.findOne(targetUserId).profile.accessFriends
+		targetUser = Meteor.users.findOne(targetUserId)
+		var friends = targetUser.profile.accessFriends
 		var friendsNeeded = Meteor.users.find({'profile.access': {$in: ['normal', 'admin']}}).count() / 5
 		if (friends.length > friendsNeeded || friendsNeeded <= 1) {
 			console.log("friend accepted")
